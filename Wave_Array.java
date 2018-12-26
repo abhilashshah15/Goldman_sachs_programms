f655bfae0632a72d514a35bd44c07f053458c95b
System Administrator <root@abhilashs-MacBook-Air.local>
2018-12-27 01:12:33 +0530

    added

---
+++ Wave_Array.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Wave_Array{
+
+public static void main(String args[]){
+
+Scanner s = new Scanner(System.in);
+
+int test = s.nextInt();
+
+while(test!=0){
+	int size = s.nextInt();
+	int ar [] = new int [size];
+	for(int i=0; i<ar.length;i++){
+		ar[i] = s.nextInt();	
+		}
+	
+	for(int i=0; i<ar.length; i =i+2){
+		
+		if(i>0 && ar[i-1]>ar[i]){
+			swap(ar,i-1,i);
+		}
+		
+		if(i<ar.length-1 && ar[i+1]>ar[i]){
+                       swap(ar,i+1,i);
+                } 
+		
+	}
+	for(int i=0;i<ar.length;i++){
+		System.out.print(ar[i]+" ");
+	}
+
+	System.out.println();
+	test --;
+}
+
+
+}
+
+private static void swap(int[] ar, int i, int i2) {
+	
+	int temp = ar[i];
+	ar[i] = ar[i2];
+	ar[i2] = temp;
+	
+	
+}
+
+
+}
+
