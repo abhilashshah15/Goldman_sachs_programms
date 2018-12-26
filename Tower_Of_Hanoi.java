9254b64b8e411bf3ac863e0efcac0db17819e372
System Administrator <root@abhilashs-MacBook-Air.local>
2018-12-26 23:24:36 +0530

    added

---
+++ Tower_Of_Hanoi.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class TOH{
+
+public static void main (String Args[]){
+
+	Scanner s = new Scanner(System.in);
+	int test = s.nextInt();
+	while(test!=0){
+	
+	int n = s.nextInt();
+	towerOfHanoi(n,'A','B','C');
+	test--;
+	
+	}
+
+
+	}
+
+static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod){
+
+	if(n==1){
+		System.out.println("move Disk 1 from "+ from_rod + " to "+ to_rod);
+		}
+
+	towerOfHanoi(n-1,from_rod,aux_rod,to_rod);
+	System.out.println("move Disk "+n+" from " +from_rod+ " to " + to_rod);
+	towerOfHanoi(n-1,aux_rod,to_rod,from_rod);
+
+}
+
+
+}
+
